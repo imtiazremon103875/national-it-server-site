3f9a2ae5a94fe42ffa32ad090490b4c0c3715ea4
imtiaz remon <imtiazremon10375@gmail.com>
2022-05-23 14:00:41 +0600

    parchase page done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require("dotenv").config();
 const jwt = require('jsonwebtoken');
@@ -14,15 +14,53 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zs9u9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = authHeader.split('')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "forbidden access" })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
+
 async function run() {
     try {
         await client.connect();
         const partsCollection = client.db('national-computer').collection('parts')
+        const userCollection = client.db('national-computer').collection('users')
+        const bookingCollection = client.db('national-computer').collection('bookings')
 
         app.get('/part', async (req, res) => {
             const query = {}
-            const parts = await partsCollection.find(query).toArray()
+            const parts = await partsCollection.find(query).limit(6).toArray()
             res.send(parts)
+        });
+
+        app.get('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const singlePart = await partsCollection.findOne(query)
+            res.send(singlePart);
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' })
+            res.send({ result, token });
         })
 
     }
