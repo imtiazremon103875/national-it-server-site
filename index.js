e6668e7ebcd73a1d14d23ad6688e4d1aa348a1be
imtiaz remon <imtiazremon10375@gmail.com>
2022-05-27 02:16:12 +0600

    payment order done

--- index.js
+++ index.js
@@ -164,6 +164,13 @@ async function run() {
             res.send(allOrders)
         })
 
+        app.get('/order/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const order = await orderCollection.findOne(query)
+            res.send(order)
+        })
+
     }
     finally {
 
